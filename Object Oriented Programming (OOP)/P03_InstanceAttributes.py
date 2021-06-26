156279713e37e7e17c4f8827fbd99996e3ff8f36
Edgar Barrera <80984699+hellocastillo@users.noreply.github.com>
2021-06-26 09:18:55 -0500

    Update P03_InstanceAttributes.py

--- Object Oriented Programming (OOP)/P03_InstanceAttributes.py
+++ Object Oriented Programming (OOP)/P03_InstanceAttributes.py
@@ -1,15 +1,15 @@
-# Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
-# Copyright (c) 2021 Edgar Barrera
+# Edgar Castillo / Github: https://github.com/hellocastillo
+# Copyright (c) 2021 Edgar Castillo
 
-#In this example we will be seeing how instance Attributes are used
-#Instance attributes are accessed by: object.attribute
-#Attributes are looked First in the instance and THEN in the class
+# In this example we will be seeing how instance Attributes are used
+# Instance attributes are accessed by: object.attribute
+# Attributes are looked First in the instance and THEN in the class
 
 import random
 class Vehicle():
     #Class Methods/ Attributes
     def type(self):
-        #NOTE: This is not a class attribute as the variable is binded to self. Hence it becomes
+        # NOTE: This is not a class attribute as the variable is binded to self. Hence it becomes
         #instance attribute
         self.randomValue = random.randint(1,10) #Setting the instance attribute
 

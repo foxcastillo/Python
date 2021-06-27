349a05160cbab25b75bbe28a2b500c20b3d18a0a
Edgar Barrera <80984699+hellocastillo@users.noreply.github.com>
2021-06-27 14:36:08 -0500

    Update P11_Property decorators.py

--- Object Oriented Programming (OOP)/P11_Property decorators.py
+++ Object Oriented Programming (OOP)/P11_Property decorators.py
@@ -1,11 +1,11 @@
 # Edgar Barrera / Github: https://github.com/EdgarCastillo101/EdgarCastillo101
 # Copyright (c) 2021 Edgar Barrera
 
-#This shows the usage of property decorators
+# This shows the usage of property decorators
 
-#Python @property is one of the built-in decorators. The main purpose of any decorator is to change your class methods or attributes in such a way so that the users neeed not make any additional changes in their code.
+# Python @property is one of the built-in decorators. The main purpose of any decorator is to change your class methods or attributes in such a way so that the users neeed not make any additional changes in their code.
 
-#Without property decorators
+# Without property decorators
 
 class BankAccount:
     def __init__(self,name,balance):
@@ -14,12 +14,12 @@ class BankAccount:
         self.total= self.name+ " has "+self.balance+ " dollars in the account"
 
 user1=BankAccount("Elon Musk","10000")
-user1.name="Tim cook"
+user1.name="Edgar Castillo"
 print(user1.name)
 print(user1.total)
 
-# Output: Tim cook
-#         Elon Musk has 10000 dollars in the account
+# Output: Edgar Castillo
+# Elon Musk has 10000 dollars in the account
 
 
 #With property decorators
@@ -33,9 +33,9 @@ class BankAccount:
         return self.name+ " has "+self.balance+ " dollars in the account"
 
 user1=BankAccount("Elon Musk","10000")
-user1.name="Tim cook"
+user1.name="Edgar Castillo"
 print(user1.name)
 print(user1.total)
 
-#Output: Tim cook
-#        Tim cook has 10000 dollars in the account
+#Output: Edgar Castillo
+#        Edgar Castillo has 10000 dollars in the account
